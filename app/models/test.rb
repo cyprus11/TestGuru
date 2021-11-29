b21bbd892b06582699c7e95da7c7c3e3368809b3
Alexey <alexx10@mail.ru>
2021-11-29 23:23:55 +0300

    lesson_4 add data to seed, create new model for tests and users

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  has_many :users_tests
+  has_many :users, through: :users_tests
+  has_many :questions
+  belongs_to :category
+
+  scope :sorted_tests, -> (category_name) { includes(:category).where(category: {title: category_name}).order(title: :desc)}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# users
+[
+  { nickname: "davis.schuster", email: "eden@smith.us" },
+  { nickname: "traci.white", email: "brittny@boscorodriguez.name" },
+  { nickname: "coy", email: "leonarda@marvin.name" },
+  { nickname: "zane.lind", email: "hailey@cassinbrekke.com" },
+  { nickname: "jackeline.dickens", email: "tresa@leschharvey.ca" },
+  { nickname: "eleanor_vandervort", email: "lovie.pollich@wolf.name" }
+].each do |user|
+  User.create(user)
+end
+
+# categories
+[
+  {title: "Frontend develop"},
+  {title: "Backend develop"},
+  {title: "Mobile develop"}
+].each do |category|
+  Category.create(category)
+end
+
+# test
+[
+  {title: "Test 1", level: rand(5), category: Category.order(Arel.sql('RANDOM()')).first},
+  {title: "Test 2", level: rand(5), category: Category.order(Arel.sql('RANDOM()')).first},
+  {title: "Test 3", level: rand(5), category: Category.order(Arel.sql('RANDOM()')).first},
+  {title: "Test 4", level: rand(5), category: Category.order(Arel.sql('RANDOM()')).first},
+  {title: "Test 5", level: rand(5), category: Category.order(Arel.sql('RANDOM()')).first}
+].each do |test|
+  Test.create(test)
+end
+
+# questions
+Test.all.each do |test|
+  4.times do |i|
+    test.questions.create(body: "Вопрос #{i}")
+  end
+end
+
+# answers
+Question.all.each do |question|
+  4.times do |i|
+    question.answers.create(body: "Ответ #{i}")
+  end
+end
+
+# add tests to users
+User.all.each do |user|
+  3.times do
+    user.tests << Test.order(Arel.sql('RANDOM()')).first
+  end
+end
+
