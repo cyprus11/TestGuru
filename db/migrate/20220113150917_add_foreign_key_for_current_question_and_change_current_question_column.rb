b0e038943b722075b2efa61bfaa776aa9a62817d
Alexey <alexx10@mail.ru>
2022-01-13 22:29:04 +0300

    lesson_9 change default value

--- db/migrate/20220113150917_add_foreign_key_for_current_question_and_change_current_question_column.rb
+++ db/migrate/20220113150917_add_foreign_key_for_current_question_and_change_current_question_column.rb
@@ -2,5 +2,6 @@ class AddForeignKeyForCurrentQuestionAndChangeCurrentQuestionColumn < ActiveReco
   def change
     add_foreign_key :test_passages, :questions, column: :current_question_id
     change_column :test_passages, :current_question_id, :integer, null: true
+    change_column_default :test_passages, :current_question_id, nil
   end
 end
