266a61ff045629330f9aa701416575b913bc5911
Alexey <alexx10@mail.ru>
2022-02-08 15:12:42 +0300

    lesson_17 fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   # Email configuration
-  config.action_mailer.default_url_options = { host: "tav-test-guru.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "tav-test-guru.herokuapp.com" }
   config.action_mailer.delivery_method = :mailjet
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
