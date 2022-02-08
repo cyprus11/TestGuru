f2ee28cb830dc3bb541089450b156dd78314f61d
Alexey <alexx10@mail.ru>
2022-02-08 15:05:41 +0300

    lesson_17 add mailjet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,16 +68,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   # Email configuration
-  config.action_mailer.default_url_options = { host: "afternoon-meadow-12099.herokuapp.com" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            Rails.application.credentials.gmail[:username],
-    password:             Rails.application.credentials.gmail[:password],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_url_options = { host: "tav-test-guru.herokuapp.com/" }
+  config.action_mailer.delivery_method = :mailjet
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   user_name:            Rails.application.credentials.gmail[:username],
+  #   password:             Rails.application.credentials.gmail[:password],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
